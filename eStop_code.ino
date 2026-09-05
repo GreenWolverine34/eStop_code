e98e3b5b2bb1975bbc8c24d1124a02f2f4178761
Kabir Kaul <kkaul29@student.rthighschool.org>
2026-09-05 14:05:01 -0400

    Update eStop_code.ino

--- eStop_code.ino
+++ eStop_code.ino
@@ -1,27 +1,32 @@
 #include <HijelHID_BLEKeyboard.h>
-#include <WiFi.h>
 
-HijelHID_BLEKeyboard bleKeyboard("Robotics Button", "Custom", 67);
+// 1. Fixed missing quotes for the device name and manufacturer strings
+HijelHID_BLEKeyboard bleKeyboard("Robotics Button", "Custom", 67); 
 
 const int estopPin = 4;
 const int switchPin = 14;
-
 bool debounce = false;
 unsigned long waitTime = 40;
 
 void setup() {
   Serial.begin(115200);
   
-  WiFi.disconnect(true);
-  WiFi.mode(WIFI_OFF);
+  // REMOVED WiFi.mode(WIFI_OFF) to prevent radio chip shutdown
   
   pinMode(estopPin, INPUT_PULLUP);
   pinMode(switchPin, INPUT_PULLUP);
   
-  bleKeyboard.begin();
+  bleKeyboard.begin(); 
+  Serial.println("BLE Keyboard Started. Advertising...");
 }
 
 void loop() {
+  // SAFETY: Only monitor buttons if a device is actually paired and connected
+  if (!bleKeyboard.isConnected()) {
+    delay(100); 
+    return;
+  }
+
   bool switchPressed = (digitalRead(switchPin) == HIGH);
   bool estopPressed = (digitalRead(estopPin) == HIGH);
 
@@ -30,20 +35,21 @@ void loop() {
     bool dualPressConfirmed = false;
 
     while (millis() - windowStart < waitTime) {
-      if (digitalRead(switchPin) ==HIGH && digitalRead(estopPin) == HIGH) {
+      if (digitalRead(switchPin) == HIGH && digitalRead(estopPin) == HIGH) {
         dualPressConfirmed = true;
         break;
       }
     }
 
     if (dualPressConfirmed) {
+      // 2. Fixed character notation typos using single quotes
       bleKeyboard.press((uint8_t)'[');
       bleKeyboard.press((uint8_t)']');
-      bleKeyboard.press((uint8_t)'\\');
+      bleKeyboard.press((uint8_t)'\\'); 
     } else {
       bleKeyboard.press(KEY_RETURN);
     }
-    
+
     debounce = true;
     delay(100);
     bleKeyboard.releaseAll();
